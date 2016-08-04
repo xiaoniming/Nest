8c4c07ed65ac82cc47b0d3c70779ddcb4357bde5
ni <nichangnm@gmail.com>
2016-08-05 01:01:24 +0800

    20160805

--- app/src/main/java/com/example/ni/nest/MainActivity.java
+++ app/src/main/java/com/example/ni/nest/MainActivity.java
@@ -1,23 +1,27 @@
 package com.example.ni.nest;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+import com.example.ni.nest.Http.javabeans.UserCommentResponse;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final static String TAG = "MainActivity----->";
+    HttpService mHttpService;
+
+    Button mScanButton;
+    Button mBookButton;
+    Button mFeedbackButton;
+
+    private static void L(String msg) {
+        Log.d(TAG, msg);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,45 +29,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final TextView mMessageWindow = (TextView) findViewById(R.id.tv_show);
-        final EditText mInputPanle = (EditText) findViewById(R.id.edit_input);
-        final Button mSendButton = (Button) findViewById(R.id.button_send);
 
-        HttpUtils.sendHttpRequestGet("http://192.168.0.118:8080/NestServer/TestServlet", new HttpCallbackListener() {
-            @Override
-            public void onFinish(String response) {
-                Log.v("mainactivity----->", response);
-                 mMessageWindow.append(response + "\n");//DO NOT DO THIS!! DubThread not UI
-            }
+        mScanButton = (Button) findViewById(R.id.bt_scan);
+        mBookButton = (Button) findViewById(R.id.bt_book);
+        mFeedbackButton = (Button) findViewById(R.id.bt_feedback);
+        mScanButton.setOnClickListener(this);
+        mBookButton.setOnClickListener(this);
+        mFeedbackButton.setOnClickListener(this);
 
-            @Override
-            public void onError(Exception e) {
-                //mInputPanle.setText("Error: " + e.getMessage());
-                Log.v("mainactivity----->", "error " + e.getMessage());
-            }
-        });
+
+        mHttpService = HttpService.get();
+        String url = "http://192.168.0.118:8080/NestServer/TestServlet";
+        mHttpService.executeJsonGetAsync(url, UserCommentResponse.class, new
+                HttpCallbackListener<UserCommentResponse>() {
+                    @Override
+                    public void onFinish(UserCommentResponse userCommentResponse) {
+                        Log.v("mainactivity----->", userCommentResponse.toString());
+                    }
+
+                    @Override
+                    public void onFail(String code, String msg) {
+                        Log.v("mainactivity----->", "error code" + code + "  errMsg: " + msg);
+                    }
+                });
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (v.getId()) {
+            case R.id.bt_book:
+                //TODO
+                break;
+            case R.id.bt_scan:
+                int requestCode = Cons.REQUEST_SCAN;
+                Intent intent = new Intent(this, ScaneActivity.class);
+                intent.putExtra(Cons.ARG_ACTION, Cons.ACTION_CAPTURE);
+                startActivityForResult(intent, requestCode);
+                break;
+            case R.id.bt_feedback:
+                //TODO
+                break;
+            default:
+                break;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case Cons.REQUEST_SCAN:
+                if (resultCode == RESULT_OK) {
+
+                    String authCode = data.getStringExtra(Cons.ARG_SCAN_RESULT);
+                    //TODO Print userInfo
+                    L("Scan succeed: " + authCode);
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
